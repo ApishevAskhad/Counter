b84e4520a203f26e7cdf432e12f68a0fadb003ed
ApishevAskhad <aaa_mt@bk.ru>
2023-05-07 22:46:41 +0300

    Завершение усложненной задачи счетчика (#5)
    * добавленна основная верстка экрана (#2)
    
    * добавлена обработка кнопок +, - и сброс (#3)
    
    * реализовано обновление истории операций; (#4)
    
    для отображения времени используется DateFormatter;

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -11,14 +11,49 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var counterLabel: UILabel!
     
-    @IBOutlet weak var incrementButton: UIButton!
+    @IBOutlet weak var historyTextView: UITextView!
     
     private var counter: Int = 0
     
-    @IBAction func incrementButtonPressed(_ sender: Any) {
-        counter += 1
-        counterLabel.text = "Значение счетчика: \(counter)"
+    private let COUNTER_MAX_VALUE = 100
+    
+    private let COUNTER_MIN_VALUE = 0
+    
+    private let DELTA = 1
+    
+    @IBAction func incrementButtonClicked(_ sender: Any) {
+        if (counter + DELTA < COUNTER_MAX_VALUE) {
+            counter += DELTA
+            updateCounter()
+            updateHistory("значение изменено на +\(DELTA)")
+        }
     }
     
+    @IBAction func decrementButtonClicked(_ sender: Any) {
+        if (counter - DELTA >= COUNTER_MIN_VALUE) {
+            counter -= DELTA
+            updateCounter()
+            updateHistory("значение изменено на -\(DELTA)")
+        } else {
+            updateHistory("попытка уменьшить значение счетчика ниже \(COUNTER_MIN_VALUE)")
+        }
+    }
+    
+    @IBAction func resetButtonClicked(_ sender: Any) {
+        counter = 0
+        updateCounter()
+        updateHistory("значение сброшено")
+    }
+    
+    private func updateCounter() {
+        counterLabel.text = "\(counter)"
+    }
+    
+    private func updateHistory(_ message: String) {
+        let currentDate = Date()
+        let formatter = DateFormatter()
+        formatter.timeStyle = .medium
+        formatter.dateStyle = .medium
+        historyTextView.text = "\(formatter.string(from: currentDate)): \(message)"
+    }
 }
-
