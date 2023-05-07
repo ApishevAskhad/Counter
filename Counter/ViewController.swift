97906c141bb8b64909a93547d986fe3bb33e09c1
ApishevAskhad <aaa_mt@bk.ru>
2023-05-07 22:25:25 +0300

    добавлена обработка кнопок +, - и сброс (#3)

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -13,12 +13,36 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var historyTextView: UITextView!
     
-    @IBOutlet weak var decrementButton: UIButton!
-    
-    @IBOutlet weak var incrementButton: UIButton!
-    
-    @IBOutlet weak var resetButton: UIButton!
-    
     private var counter: Int = 0
+    
+    private let COUNTER_MAX_VALUE = 100
+    
+    private let COUNTER_MIN_VALUE = 0
+    
+    private let DELTA = 1
+    
+    @IBAction func incrementButtonClicked(_ sender: Any) {
+        if (counter + DELTA < COUNTER_MAX_VALUE) {
+            counter += DELTA
+            updateCounter()
+        }
+    }
+    
+    @IBAction func decrementButtonClicked(_ sender: Any) {
+        if (counter - DELTA >= COUNTER_MIN_VALUE) {
+            counter -= DELTA
+            updateCounter()
+        }
+    }
+    
+    @IBAction func resetButtonClicked(_ sender: Any) {
+        counter = 0
+        updateCounter()
+    }
+    
+    private func updateCounter() {
+        counterLabel.text = "\(counter)"
+    }
+        
 }
 
